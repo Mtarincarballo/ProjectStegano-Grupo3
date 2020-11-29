e23a43173e0032e5fc298e1425e1fb6f8c430c67
1damFer <66639525+1damFer@users.noreply.github.com>
2020-11-29 18:54:02 +0100

    AÑADO el MENU
    Fernando: añado el commit del menu al programa principal

--- PruebaDefinitiva.py
+++ PruebaDefinitiva.py
@@ -61,7 +61,7 @@ print(decrypt('new.png', locs))
 '''
 
 ## Pasar imagen a gris
-def pasarAgris():
+def op3():
     imagen = cv2.imread('proyimag1T.png')
     print("Convirtiendo la imagen a escala de grises...")
     img_engris = cv2.cvtColor(imagen, cv2.COLOR_BGR2GRAY)
@@ -70,3 +70,38 @@ def pasarAgris():
     cv2.imshow('Escala de grises', img_engris)
     cv2.waitKey(0)
     cv2.destroyAllWindows()
+
+# AQUI EMPIEZA EL MENU
+
+def menu():
+    print("Selecciona una opción")
+    print("\t1 - [1] Insertar mensaje oculto en una imagen")
+    print("\t2 - [2] Extraer mensaje oculto de una imagen")
+    print("\t3 - [3] Convertir la imagen a escala de grises")
+    print("\t4 - [4] Salir")
+
+menu()
+opcion = int(input("Introduce la opción deseada: "))
+
+while opcion != 4:
+    if opcion == 1:
+        print("Ha elegido: Insertar mensaje oculto en una imagen")
+        op1()
+    elif opcion == 2:
+        print("Ha elegido: Extraer mensaje oculto de una imagen")
+        op2()
+    elif opcion == 3:
+        print("Ha elegido: Convertir la imagen a escala de grises")
+        op3()
+
+        '''SI EL USUARIO ELIGE (4) SE CIERRA EL BUCLE Y APARECE UN MENSAJE DE ERROR, VUELVE A APARECER EL MENU'''
+    else:
+        print("Opción no valida")
+    print()
+    menu()
+    opcion = int(input("Introduce la opción deseada: "))
+
+print("Gracias por usar el programa. Adios.")
+
+
+
